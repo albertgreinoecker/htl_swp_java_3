16c3101befa0c631b37a31a98332d18020fa6614
albert.greinoecker@gmail.com <albert.greinoecker@gmail.com>
2022-10-13 11:04:30 +0200

    Zufällige Positionierung

--- htl_swp_java_3/src/at/ac/htlinn/ex02_memory/Playground.java
+++ htl_swp_java_3/src/at/ac/htlinn/ex02_memory/Playground.java
@@ -1,21 +1,58 @@
 package at.ac.htlinn.ex02_memory;
 
+import java.util.Iterator;
+
 public class Playground {
 	private Card[][] cards; //(0,0) links oben
 	private int whosOnTurn; //0..Spieler 1, 1...Spieler2, ...
 	private int[] score; //Die Anzahl der Paerchen pro Spieler
 	private int nrJoker;
 	private int nrPairs;
-	
-	public Playground(int whosOnTurn, int nrJoker, int nrPairs) {
-		this.whosOnTurn = whosOnTurn;
+
+	/**
+	 * @param x Groesse in x-Richtung
+	 * @param y Groesse in y-Richtung
+	 * @param nrJoker Anzahl der Jokerkarten
+	 * @param nrPairs Anzahl der zu erzeugenden Paare
+	 * @param nrPlayers wie viele Spieler nehmen Teil
+	 */
+	public Playground(int x, int y,  int nrJoker, int nrPairs, int nrPlayers) {
 		this.nrJoker = nrJoker;
 		this.nrPairs = nrPairs;
+		score = new int[nrPlayers];
+		cards = new Card[x][y];
+		init();
 	}
 	
+	/**
+	 * Erzeuge zufaellig ein mit Karten besetztes Spielfeld
+	 */
 	public void init()
 	{
+		//Alle Felder zuerst auf EmptyCard setzen
+		for (int i = 0; i < cards.length; i++) {
+			for (int j = 0; j < cards[i].length; j++) {
+				cards[i][j] = new EmptyCard();
+			}
+		}
 		
+		for (int i = 0; i < nrPairs; i++) {
+			placeRegularCard(i);
+			placeRegularCard(i);
+		}
+		
+	}
+	
+	private void placeRegularCard(int c)
+	{
+		int x = 0;
+		int y = 0;
+		do //solange kein freier Platz gefunden, ziehe neue x und y
+		{
+			x = (int)(Math.random()*cards.length);
+			y = (int)(Math.random()*cards[0].length);
+		} while (!(cards[x][y] instanceof EmptyCard));
+		cards[x][y] = new RegularCard(c);
 	}
 	
 	/**
--- htl_swp_java_3/src/at/ac/htlinn/ex02_memory/RegularCard.java
+++ htl_swp_java_3/src/at/ac/htlinn/ex02_memory/RegularCard.java
@@ -1,5 +1,9 @@
 package at.ac.htlinn.ex02_memory;
 
 public class RegularCard extends Card {
+	private int value;
 
+	public RegularCard(int value) {
+		this.value = value;
+	}
 }
