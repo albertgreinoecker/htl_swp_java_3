46a1be4e60cc30f40400a513ace545c64bb468d1
albert.greinoecker@gmail.com <albert.greinoecker@gmail.com>
2022-10-20 11:27:04 +0200

    gettter und setter für visible

--- htl_swp_java_3/src/at/ac/htlinn/ex02_memory/Card.java
+++ htl_swp_java_3/src/at/ac/htlinn/ex02_memory/Card.java
@@ -3,6 +3,16 @@ package at.ac.htlinn.ex02_memory;
 public abstract class Card {
 	protected boolean visible;
 
+	
+	
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
 	@Override
 	public String toString() {
 		return visible ? "_" : "*";
