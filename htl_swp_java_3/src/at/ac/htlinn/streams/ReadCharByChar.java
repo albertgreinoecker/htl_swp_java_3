f738c683a7ccb22736fdc5a81e99c9d61d83bbcf
albert.greinoecker@gmail.com <albert.greinoecker@gmail.com>
2023-03-23 10:29:55 +0100

    Read char by char

---
+++ htl_swp_java_3/src/at/ac/htlinn/streams/ReadCharByChar.java
@@ -0,0 +1,20 @@
+package at.ac.htlinn.streams;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadCharByChar {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner s = new Scanner(new File("testdata/p1.txt"));
+		s.useDelimiter("\\Z");  //Trennzeichen ist das letzte Zeichen in der Datei
+		String content = s.next(); 
+		for (char c : content.toCharArray())
+		{
+			System.out.println(c);
+		}
+		s.close();
+	}
+
+}
