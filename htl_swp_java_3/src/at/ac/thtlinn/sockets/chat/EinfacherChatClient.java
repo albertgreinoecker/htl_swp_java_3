a104e8d4cbb2b12192fce378e0f5c1012f0ce633
albert.greinoecker@gmail.com <albert.greinoecker@gmail.com>
2023-05-24 15:12:38 +0200

    added chat server and client

---
+++ htl_swp_java_3/src/at/ac/thtlinn/sockets/chat/EinfacherChatClient.java
@@ -0,0 +1,94 @@
+package at.ac.thtlinn.sockets.chat;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
+public class EinfacherChatClient extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+
+	private JTextArea eingehend;
+	private JTextField ausgehend;
+	private BufferedReader reader;
+	private PrintWriter writer;
+	private Socket sock;
+
+	public EinfacherChatClient(String title) {
+		super(title);
+		JPanel hauptPanel = new JPanel();
+		eingehend = new JTextArea(15, 20);
+		eingehend.setLineWrap(true);
+		eingehend.setWrapStyleWord(true);
+		eingehend.setEditable(false);
+		JScrollPane fScroller = new JScrollPane(eingehend);
+		fScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		fScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		ausgehend = new JTextField(20);
+		JButton sendenButton = new JButton("Senden");
+		sendenButton.addActionListener(this);
+		hauptPanel.add(fScroller);
+		hauptPanel.add(ausgehend);
+		hauptPanel.add(sendenButton);
+
+		netzwerkEinrichten();
+		Thread readerThread = new Thread(new EingehendReader());
+
+		readerThread.start();
+		add(BorderLayout.CENTER, hauptPanel);
+	}
+
+	private void netzwerkEinrichten() {
+		try {
+			sock = new Socket("127.0.0.1", 5000);
+			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+			reader = new BufferedReader(streamReader);
+			writer = new PrintWriter(sock.getOutputStream());
+			System.out.println("Netzwerkverbindung steht...");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public class EingehendReader implements Runnable {
+		@Override
+		public void run() {
+			String nachricht;
+			try {
+				while ((nachricht = reader.readLine()) != null) {
+					System.out.println("gelesen" + nachricht);
+					eingehend.append(nachricht + "\n");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		writer.println(ausgehend.getText());
+		writer.flush();
+		ausgehend.setText("");
+		ausgehend.requestFocus();
+	}
+	
+	public static void main(String[] args) {
+		EinfacherChatClient client = new EinfacherChatClient("Einfacher Chat-Client");
+		client.setSize(400, 500);
+		client.setVisible(true);
+	}
+}
---
+++ htl_swp_java_3/src/at/ac/thtlinn/sockets/chat/SehrEinfacherChatServer.java
@@ -0,0 +1,80 @@
+package at.ac.thtlinn.sockets.chat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class SehrEinfacherChatServer {
+	ArrayList<PrintWriter> clientAusgabeStroeme;
+
+	public class ClientHandler implements Runnable {
+		BufferedReader reader;
+		Socket sock;
+
+		public ClientHandler(Socket clientSocket) {
+			sock = clientSocket;
+			InputStreamReader isReader;
+			try {
+				isReader = new InputStreamReader(sock.getInputStream());
+				reader = new BufferedReader(isReader);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void run() {
+			String nachricht;
+			try {
+				while ((nachricht = reader.readLine()) != null) {
+					System.out.println("gelesen" + nachricht);
+					esAllenWeitersagen(nachricht);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	} // end ClientHandler
+
+	public static void main(String[] args) throws IOException {
+		new SehrEinfacherChatServer().los();
+	}
+
+	public void los() throws IOException {
+		clientAusgabeStroeme = new ArrayList<>();
+		ServerSocket serverSock = null;
+		try {
+			serverSock = new ServerSocket(5000);
+			while (true) {
+				Socket clientSocket = serverSock.accept();
+				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+				clientAusgabeStroeme.add(writer);
+				Thread t = new Thread(new ClientHandler(clientSocket));
+				t.start();
+				System.out.println("habe eine Verbindung");
+			}
+			// wenn wir hier angelangt sind, haben wir eine Verbindung
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			serverSock.close();
+		}
+	}
+
+	public void esAllenWeitersagen(String nachricht) {
+		Iterator<PrintWriter> it = clientAusgabeStroeme.iterator();
+		while (it.hasNext()) {
+			try {
+				PrintWriter writer = (PrintWriter) it.next();
+				writer.println(nachricht);
+				writer.flush();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		} // Ende der while-Schleife
+	} // esAllenWeitersagen schließen
+} // Klasse schließen
